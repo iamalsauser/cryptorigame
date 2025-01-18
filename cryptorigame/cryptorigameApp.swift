55b42d1e0d3874d9e2e9f5ceff2834d053485b77
Iamalsauser <parthsinhv4@gmail.com>
2025-01-18 23:43:59 +0530

    Initial Commit

---
+++ cryptorigame/cryptorigameApp.swift
@@ -0,0 +1,17 @@
+//
+//  cryptorigameApp.swift
+//  cryptorigame
+//
+//  Created by admin17 on 18/01/25.
+//
+
+import SwiftUI
+
+@main
+struct cryptorigameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cryptorigameTests/cryptorigameTests.swift
@@ -0,0 +1,17 @@
+//
+//  cryptorigameTests.swift
+//  cryptorigameTests
+//
+//  Created by admin17 on 18/01/25.
+//
+
+import Testing
+@testable import cryptorigame
+
+struct cryptorigameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
